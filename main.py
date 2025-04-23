f731193a54e75ab45d355e131b049067dc4dab93
Third <astroll656@gmail.com>
2025-04-24 03:55:45 +0700

    init env

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+from login.huggingface_login import HuggingFaceLogin
+from transformers import BertTokenizer, BertModel
+
+hf = HuggingFaceLogin()
+
+
+# #multilingual
+model = BertModel.from_pretrained('bert-base-multilingual-cased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+
+
+text = "Replace me by any text you'd like."
+encoded_input = tokenizer(text, return_tensors='pt')
+output = model(**encoded_input)
