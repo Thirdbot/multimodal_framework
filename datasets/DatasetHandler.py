2a86bf199e0a764e936476c00ec216035f4cb3eb
Third <astroll656@gmail.com>
2025-04-24 05:32:43 +0700

    files and placeholder for ft lang model

--- datasets/DatasetHandler.py
+++ datasets/DatasetHandler.py
@@ -1,2 +1,15 @@
-import datasets
 
+from datasets import load_dataset
+
+import os
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+
+dataset = load_dataset("oscar-corpus/OSCAR-2201",
+                        language="th", 
+                        streaming=True, # optional
+                        split="train",
+                        trust_remote_code=True) # optional, but the dataset only has a train split
+
+for d in dataset:
+    print(d) # prints documents
