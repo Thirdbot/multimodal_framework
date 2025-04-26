78d2d4748021b7161ed93b65bb55dad081792aa0
Third <astroll656@gmail.com>
2025-04-26 17:14:46 +0700

    prft merge model save and load(not working & manual)

--- models/DatasetHandler.py
+++ models/DatasetHandler.py
@@ -1,6 +1,5 @@
 
 from datasets import load_dataset
-
 import os
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
@@ -12,13 +11,14 @@ class DatasetHandler:
         self.split = split
         self.dataset = load_dataset(name,
                         language=self.language, 
-                        streaming=True, # optional
                         split=self.split,
+                        streaming=True,
                         trust_remote_code=True) # optional, but the dataset only has a train split
+        
+        
 
 
     def get_dataset(self):
         return self.dataset
-
 
 
