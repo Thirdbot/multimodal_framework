c6d4b8b482f9b7bc6bc1dce719af7e89520c74c6
Third <astroll656@gmail.com>
2025-05-06 23:06:50 +0700

    Add file for font/back end

---
+++ backend/main_backend.py
@@ -0,0 +1,1 @@
+#use flask to communicate over server and api to frontend
---
+++ frontend/main_frontend.py
@@ -0,0 +1,2 @@
+#use streamlit for demo application and visualize the data 
+#note use electron instead for multimodal data
