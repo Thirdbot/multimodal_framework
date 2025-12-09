65b438bea32e7e6b39620a9e30c126c3de5fcb6f
thirdbot <astroll656@gmail.com>
2025-12-09 17:52:28 +0700

    main example

--- main.py
+++ main.py
@@ -10,40 +10,40 @@ from modules.inference import InferenceManager
 from modules.train import FinetuneModel
 from pathlib import Path
 
-# acess_token = os.environ.get("hf_token")
+acess_token = os.environ.get("hf_token")
 
-# api = HfApi()
-# variable = Variable()
-# DMConfig_DIR = variable.DMConfig_DIR
+api = HfApi()
+variable = Variable()
+DMConfig_DIR = variable.DMConfig_DIR
 
-# DMConfig_DIR.mkdir(parents=True, exist_ok=True)
-# SavedConfigsfile= variable.SAVED_CONFIG_Path
-# SavedConfigsfile.touch(exist_ok=True)
+DMConfig_DIR.mkdir(parents=True, exist_ok=True)
+SavedConfigsfile= variable.SAVED_CONFIG_Path
+SavedConfigsfile.touch(exist_ok=True)
 
-# setcard = ApiCardSetup()
+setcard = ApiCardSetup()
 
-# list_models = api.list_models(model_name='Qwen/Qwen1.5-0.5B-Chat',limit=1,gated=False)
-# # list_models = api.list_models(model_name='Qwen/Qwen3-4B-Instruct-2507',limit=1,gated=False)
-# # list_datasets = api.list_datasets(dataset_name='QHQK/conversation_hall_binary_v1',limit=1,gated=False)
+list_models = api.list_models(model_name='Qwen/Qwen1.5-0.5B-Chat',limit=1,gated=False)
+# list_models = api.list_models(model_name='Qwen/Qwen3-4B-Instruct-2507',limit=1,gated=False)
+# list_datasets = api.list_datasets(dataset_name='QHQK/conversation_hall_binary_v1',limit=1,gated=False)
 
-# # list_datasets = api.list_datasets(dataset_name='ThucPD/coco-qa-vi',limit=1,gated=False)
-# list_datasets = api.list_datasets(dataset_name='waltsun/MOAT',limit=1,gated=False)
-# # list_datasets = api.list_datasets(dataset_name="FreedomIntelligence/medical-o1-reasoning-SFT",limit=1,gated=False)
-
-
-# #set new list to download
-# list_download = setcard.set(list_models,list_datasets)
-
-# downloading = DataLoader()
-
-# #download from datacard
-# downloading.run(list_download)
+# list_datasets = api.list_datasets(dataset_name='ThucPD/coco-qa-vi',limit=1,gated=False)
+list_datasets = api.list_datasets(dataset_name='waltsun/MOAT',limit=1,gated=False)
+# list_datasets = api.list_datasets(dataset_name="FreedomIntelligence/medical-o1-reasoning-SFT",limit=1,gated=False)
 
 
-# # #finetune model
-# finetune = Manager()
+#set new list to download
+list_download = setcard.set(list_models,list_datasets)
 
-# finetune.dataset_prepare(list_download)
+downloading = DataLoader()
+
+#download from datacard
+downloading.run(list_download)
+
+
+# #finetune model
+finetune = Manager()
+
+finetune.dataset_prepare(list_download)
 
 Ft = FinetuneModel()
 Ft.finetune_model()
